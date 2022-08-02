bd2a7ab99fd8dffe1c75dc378f52aa85df96144b
Adewuyi Adeyanju <wuyiadeyanju@yahoo.com>
2022-08-02 19:48:40 +0100

    Run Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="jagacodes/projectml:v1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run projectml\
+    --image=$dockerpath\
+    --port=80 --labels app=projectml
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward projectml 8000:80
